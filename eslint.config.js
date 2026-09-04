cf8cbf4bd5c6cdae0de52b3eec3345190d4e7224
ezlixp <henrygoals2007@gmail.com>
2026-09-03 21:46:07 -0400

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -1,89 +1,35 @@
-import { defineConfig } from "lint-staged/config"
+import eslint from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier"
+import globals from "globals"
 
-export default defineConfig([
+export default tseslint.config(
     {
-        env: {
-            commonjs: true,
-            es2021: true,
-            node: true,
-        },
-        parser: "@typescript-eslint/parser",
-        plugins: ["@typescript-eslint"],
-        extends: [
-            "eslint:recommended",
-            "prettier",
-            "plugin:@typescript-eslint/recommended",
-            "plugin:@typescript-eslint/recommended-requiring-type-checking",
-        ],
-        parserOptions: {
-            project: ["./tsconfig.json"],
-        },
-        globals: {
-            client: "writable",
+        ignores: ["node_modules/", "dist/", "build/", "coverage/", ".next/", "*.log"],
+    },
+
+    eslint.configs.recommended,
+
+    ...tseslint.configs.recommendedTypeChecked,
+
+    {
+        languageOptions: {
+            globals: {
+                ...globals.commonjs,
+                ...globals.es2021,
+                ...globals.node,
+                client: "writable",
+            },
+            parserOptions: {
+                project: ["./tsconfig.json"],
+                tsconfigRootDir: import.meta.dirname,
+            },
         },
         rules: {
             "@typescript-eslint/no-explicit-any": "off",
             "@typescript-eslint/no-unsafe-argument": "off",
         },
-        ignores: [
-            // Logs
-            "logs",
-            "*.log",
-            "npm-debug.log*",
-            "yarn-debug.log*",
-            "yarn-error.log*",
-            "lerna-debug.log*",
-
-            // Diagnostic reports
-            "report.[0-9]*.[0-9]*.[0-9]*.[0-9]*.json",
-
-            // Runtime data
-            "pids",
-            "*.pid",
-            "*.seed",
-            "*.pid.lock",
-
-            // Directory for instrumented libs
-            "lib-cov",
-
-            // Coverage directory
-            "coverage",
-            "*.lcov",
-            ".nyc_output",
-
-            // Build & dependencies
-            ".grunt",
-            "bower_components",
-            ".lock-wscript",
-            "build/Release",
-            "node_modules/",
-            "jspm_packages/",
-            "typings/",
-            "*.tsbuildinfo",
-
-            // Caches & env
-            ".npm",
-            ".eslintcache",
-            ".rpt2_cache/",
-            ".rts2_cache_cjs/",
-            ".rts2_cache_es/",
-            ".rts2_cache_umd/",
-            ".node_repl_history",
-            "*.tgz",
-            ".yarn-integrity",
-            ".env",
-            ".env.test",
-            ".cache",
-
-            // Framework outputs
-            ".next",
-            ".nuxt",
-            "dist",
-            ".vuepress/dist",
-            ".serverless/",
-            ".fusebox/",
-            ".dynamodb/",
-            ".tern-port",
-        ],
     },
-])
+
+    eslintConfigPrettier,
+)
